09720410f5916c1eff46630440bd7526782a6f03
Domi <Domi>
2024-02-23 14:53:46 +0100

    delete error

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:paloozatestapp/home/screens/loading_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:paloozatestapp/home/screens/new_enterscreen.dart';
 import 'services/firebase_options.dart';
