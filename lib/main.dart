e4a8b71f042b03adb538fe6c85726ced49d8acb2
Domi <Domi>
2024-02-23 12:35:34 +0100

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:paloozatestapp/screens/loadingscreen.dart';
+import 'package:paloozatestapp/models/loadingscreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'services/firebase_options.dart';
 
